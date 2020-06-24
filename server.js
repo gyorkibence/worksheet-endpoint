d6899cf0240eb982d534804c91b231fe6e436008
balazs <balazs@chp-MacBook-Pro.local>
2020-06-24 23:19:20 +0200

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const data = require('./data');
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('hellobello');
